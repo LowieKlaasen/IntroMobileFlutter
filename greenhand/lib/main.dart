59e8da6b1d00b06a08a21b3a8d525c0cd69b8b5d
Lowie <lowie.klaasen@student.ap.be>
2025-04-23 14:31:25 +0200

    starting firebase auth

--- greenhand/lib/main.dart
+++ greenhand/lib/main.dart
@@ -2,12 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
+
+final FirebaseAuth auth = FirebaseAuth.instance;
 
 // Firestore Test
 void testFirestoreWrite() async {

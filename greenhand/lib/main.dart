91acb40ec51e071b7f455d533043c0c9e93e624d
Lowie <lowie.klaasen@student.ap.be>
2025-04-24 11:21:33 +0200

    android version works

--- greenhand/lib/main.dart
+++ greenhand/lib/main.dart
@@ -12,6 +12,27 @@ void main() async {
 
 final FirebaseAuth auth = FirebaseAuth.instance;
 
+Future<void> createUserWithEmailAndPassword(
+  String email,
+  String password,
+) async {
+  try {
+    UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    // print('User created: ${userCredential.user!.uid}');
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      // print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      // print('The account already exists for that email.');
+    }
+  } catch (e) {
+    // print(e);
+  }
+}
+
 // Firestore Test
 void testFirestoreWrite() async {
   try {

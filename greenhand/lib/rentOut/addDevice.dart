586e54bb46021dab76a3e20e4e5f319db5814827
Lowie <lowie.klaasen@student.ap.be>
2025-04-29 22:13:07 +0200

    cleanup

--- greenhand/lib/rentOut/addDevice.dart
+++ greenhand/lib/rentOut/addDevice.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:greenhand/rentOut/rentOutDashboard.dart';
 import 'package:greenhand/services/firestoreService.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';

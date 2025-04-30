1377ec31237a32ed7dbe0c5b54a56949dd90c432
Lowie <lowie.klaasen@student.ap.be>
2025-04-30 11:51:21 +0200

    device detail screen

--- greenhand/lib/home.dart
+++ greenhand/lib/home.dart
@@ -2,7 +2,9 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:greenhand/firebase_options.dart';
+import 'package:greenhand/login/login.dart';
 import 'package:greenhand/rentOut/rentOutDashboard.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -24,83 +26,115 @@ class _HomeState extends State<Home> {
     );
   }
 
+  Future<void> signOutUser() async {
+    await FirebaseAuth.instance.signOut();
+
+    // ToDo: Fix navigating after logging out
+    if (!mounted) return;
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => LoginScreen()),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "Home",
-              style: TextStyle(
-                fontSize: 36,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
-            ),
-            SizedBox(height: 100),
-            Image(
-              image: AssetImage("assets/icon/GreenHand_Logo.png"),
-              width: screenWidth * 0.4,
-            ),
-            SizedBox(height: 100),
-            ElevatedButton(
-              onPressed: () {
-                // ToDo: Navigate
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF636B2F),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(7)),
-                ),
-                padding: EdgeInsets.symmetric(
-                  vertical: 15,
-                ), // Only vertical padding
-              ),
-              child: SizedBox(
-                width: screenWidth * 0.65, // Set a fixed width for both buttons
-                child: Center(
-                  child: Text(
-                    "Search in area",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  "Home",
+                  style: TextStyle(
+                    fontSize: 36,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
                   ),
                 ),
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => Rentoutdashboard()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color(0xFF636B2F),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.all(Radius.circular(7)),
+                SizedBox(height: 100),
+                Image(
+                  image: AssetImage("assets/icon/GreenHand_Logo.png"),
+                  width: screenWidth * 0.4,
                 ),
-                padding: EdgeInsets.symmetric(
-                  vertical: 15,
-                ), // Only vertical padding
-              ),
-              child: SizedBox(
-                width: screenWidth * 0.65, // Same fixed width for consistency
-                child: Center(
-                  child: Text(
-                    "Rent out",
-                    style: TextStyle(color: Colors.white, fontSize: 18),
+                SizedBox(height: 100),
+                ElevatedButton(
+                  onPressed: () {
+                    // ToDo: Navigate
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF636B2F),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7)),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 15,
+                    ), // Only vertical padding
+                  ),
+                  child: SizedBox(
+                    width:
+                        screenWidth *
+                        0.65, // Set a fixed width for both buttons
+                    child: Center(
+                      child: Text(
+                        "Search in area",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
                   ),
                 ),
-              ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Rentoutdashboard(),
+                      ),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF636B2F),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(7)),
+                    ),
+                    padding: EdgeInsets.symmetric(
+                      vertical: 15,
+                    ), // Only vertical padding
+                  ),
+                  child: SizedBox(
+                    width:
+                        screenWidth * 0.65, // Same fixed width for consistency
+                    child: Center(
+                      child: Text(
+                        "Rent out",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 100),
+              ],
             ),
-            SizedBox(height: 100),
-          ],
-        ),
+          ),
+          Positioned(
+            bottom: 20,
+            right: 20,
+            child: FloatingActionButton(
+              onPressed: () {
+                // ToDo: Implement logout functionality
+                print("User logged out");
+              },
+              backgroundColor: Color(0xFF636B2F),
+              child: Icon(Icons.logout, color: Colors.white),
+            ),
+          ),
+        ],
       ),
     );
   }
--- greenhand/lib/search/deviceDetail.dart
+++ greenhand/lib/search/deviceDetail.dart
@@ -1,26 +1,193 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:intl/intl.dart';
 
-class DeviceDetail extends StatelessWidget {
+class DeviceDetail extends StatefulWidget {
   final Map<String, dynamic> device;
 
   const DeviceDetail({Key? key, required this.device}) : super(key: key);
 
   @override
+  _DeviceDetailState createState() => _DeviceDetailState();
+}
+
+class _DeviceDetailState extends State<DeviceDetail> {
+  @override
+  void initState() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    super.dispose();
+  }
+
+  String formatDate(String date) {
+    try {
+      final parsedDate = DateTime.parse(date);
+      return DateFormat('MMM dd').format(parsedDate);
+    } catch (e) {
+      return "Invalid date";
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(device['name'] ?? "Device Detail"),
-        backgroundColor: Color(0xFF636B2F),
-      ),
-      body: Center(
-        child: Text(
-          "To Be Implemented",
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.grey,
+      body: Stack(
+        children: [
+          // Main Content
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Align content to the left
+              children: [
+                // Device Image
+                ClipRRect(
+                  child: Image.network(
+                    widget.device['imageUrl'] ??
+                        "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png",
+                    width: double.infinity,
+                    height: 220,
+                    fit: BoxFit.cover,
+                    errorBuilder: (context, error, stackTrace) {
+                      return Icon(
+                        Icons.broken_image,
+                        size: 100,
+                        color: Colors.grey,
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        widget.device['name'] ?? "Unknown Device",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        "€${widget.device['price'] ?? 'N/A'} per day",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                          color: Color(0xFF636B2F),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.calendar_today,
+                            size: 20,
+                            color: Colors.grey[600],
+                          ),
+                          SizedBox(width: 8),
+                          Text(
+                            "Available from ${formatDate(widget.device['startDate'])} - ${formatDate(widget.device['endDate'])}",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[600],
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        "Description",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        widget.device['description'] ??
+                            "No description available.",
+                        style: TextStyle(fontSize: 16, color: Colors.grey[800]),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        "Location",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Container(
+                        height: 150,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          color: Colors.grey[300],
+                        ),
+                        child: Center(
+                          child: Icon(
+                            Icons.map,
+                            size: 50,
+                            color: Colors.grey[600],
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Text(
+                        "Owner",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 25,
+                            backgroundImage: NetworkImage(
+                              widget.device['ownerImageUrl'] ??
+                                  "https://static.vecteezy.com/system/resources/thumbnails/003/337/584/small/default-avatar-photo-placeholder-profile-icon-vector.jpg",
+                            ),
+                          ),
+                          SizedBox(width: 16),
+                          Text(
+                            "${widget.device['user_firstName']} ${widget.device['user_lastName']}",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.grey[800],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
+          Positioned(
+            top: 20,
+            left: 10,
+            child: IconButton(
+              icon: Icon(Icons.arrow_back, color: Colors.white, size: 30),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
